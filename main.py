1498ada28e6992093d35d36676795a563862d21c
Jack Compton <90483783+jackcompton94@users.noreply.github.com>
2023-02-20 18:50:37 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import csv
+from datetime import datetime
+
+# # API calls
+# profileUrl = "https://api.chess.com/pub/player/v2j-c"
+# statsUrl = "https://api.chess.com/pub/player/v2j-c/stats"
+
+# # http responses
+# profileResponse = requests.request("GET", profileUrl)
+# statsResponse = requests.request("GET", statsUrl)
+
+# # Converting response to object
+# profileResponseObject = json.loads(profileResponse.text)
+# statsResponseObject = json.loads(statsResponse.text)
+
+# # JSON formatter
+# chessProfile = json.dumps(profileResponseObject, indent=2)
+# chessStats = json.dumps(statsResponseObject, indent=2)
+
+# print(chessProfile)
+# print(chessStats)
+
+# ---------------------------------------------------- START OF THE ACTUAL SCRIPT ---------------------------------------------------- #
+
+# Chess data config
+month = datetime.today()
+monthString = str(month.month)
+
+if int(monthString) < 10:
+    monthString = "0" + monthString
+
+gameUrl = "https://api.chess.com/pub/player/v2j-c/games/2023/" + monthString
+gameResponse = requests.request("GET", gameUrl)
+gameResponseObject = json.loads(gameResponse.text)
+chessGames = json.dumps(gameResponseObject, indent=2)
+chessJSON = gameResponse.json()
+
+# Opening file locally
+with open("/Users/jackcompton/PycharmProjects/chessDashboard/csvs/" + month.strftime("%m_%Y") + "_games.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["game_id", "white", "white_rating", "white_result", "black", "black_rating", "black_result"])
+    i = 0
+
+    for games, game in chessJSON.items():
+        for key in game:
+            white = key['white']
+            black = key['black']
+            i += 1
+
+            # Saving file locally
+            writer.writerow([i, white['username'], str(white['rating']), white['result'], black['username'], str(black['rating']), black['result']])
